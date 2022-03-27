9ee3a08fa5da967c3d4beae1e5568a533da94710
ChoSangHyeon <whtkdgusdldi@naver.com>
2022-03-27 17:09:59 +0900

    [fix] 출돌관리 및 불필요한 파일 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
 import { typeORMConfig } from './configs/typeorm.config';
 import { AuthModule } from './auth/auth.module';
-<<<<<<< HEAD
-import { ClassModule } from './class/classes.module';
-=======
 import { ClassModule } from './class/class.module';
->>>>>>> 709a134491bdcfc6d407646ffecf7c4b81d6104e
 import { ChatModule } from './chat/chat.module';
 
 @Module({
