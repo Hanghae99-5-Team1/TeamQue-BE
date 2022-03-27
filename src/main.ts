c322fa1854c18f96024cb19f598e1147694a37dc
ChoSangHyeon <94961333+ChoSangHyeon@users.noreply.github.com>
2022-03-27 20:25:46 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,13 @@ import * as config from 'config';
 import { readFileSync } from 'fs';
 
 async function bootstrap() {
-  // const httpsOptions = {
-  //   key: readFileSync('../../../etc/letsencrypt/live/noobpro.shop/privkey.pem'),
-  //   cert: readFileSync(
-  //     '../../../etc/letsencrypt/live/noobpro.shop/fullchain.pem',
-  //   ),
-  // };
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: readFileSync('../../../etc/letsencrypt/live/noobpro.shop/privkey.pem'),
+    cert: readFileSync(
+      '../../../etc/letsencrypt/live/noobpro.shop/fullchain.pem',
+    ),
+  };
+  const app = await NestFactory.create(AppModule,{ httpsOptions });
   //유효성 검사
   app.useGlobalPipes(
     new ValidationPipe({
